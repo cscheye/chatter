ec3ee4185e194c21e3a89bfdef8ada986c2d32d4
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-20 16:49:03 -0700

    rewriting router to specify filetype in header so that js files can be loaded

--- router.js
+++ router.js
@@ -1,26 +1,31 @@
 var url = require('url')
 var fs = require('fs');
+var mime = require('mime')
 
 
 var router = function(req, res) {
 
   if (req.url === '/'){
     console.log(req.url);
-    writeRes('./public/index.html');
+    writeRes('index.html');
   } else {
     console.log(req.url);
     writeRes(req.url);
   }
 
   function writeRes(filename) {
-    fs.readFile(filename, { encoding: 'utf-8' }, function(err, data) {
+    var path = "./public/" + filename;
+    console.log(path)
+    fs.readFile(path, { encoding: 'utf-8' }, function(err, data) {
       if (err) {
-        res.statusCode = 404;
-        res.write("Could not find file: " + filename)
+        serveError(res, 404)
       } else {
-        res.write(data);
+        res.writeHead(
+          200,
+          {"Content-Type" : mime.lookup(path)});
+        res.end(data);
       }
-      res.end();
+
     })
   }
 
